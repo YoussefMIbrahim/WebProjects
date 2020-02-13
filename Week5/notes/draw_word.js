2ea5eb04d3e24235d76752b747fabae9c3fb38e5
YoussefMIbrahim <kn1010az@go.minnstate.edu>
2020-02-13 13:18:32 -0600

    more stuff that doesn work

---
+++ Week5/notes/draw_word.js
@@ -0,0 +1,24 @@
+let canvas = document.querySelector("#canvas");
+let context = canvas.getContext("2d");
+
+let input = document.querySelector("#image_text");
+input.disabled = true;
+
+let image = new Image();
+image.src = "computer_cat.jpg";
+
+image.addEventListener("load" , function () {
+
+    context.drawImage(image,0,0);
+    input.disabled = false;
+});
+
+input.addEventListener("input", function () {
+
+    let text = this.value;
+
+    context.drawImage(image,0,0);
+    context.font = "40px Arial";
+    context.fillStyle = "Black";
+
+});
