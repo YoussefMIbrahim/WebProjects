d205833063074daaa4e3bd0a0b1e115ba5291c81
YoussefMIbrahim <kn1010az@go.minnstate.edu>
2020-02-16 14:31:10 -0600

    just some updates

---
+++ Week5/labs/maps.js
@@ -0,0 +1,8 @@
+bridges = [
+    {'name': 'Verrazano-Narrows Bridge','cit/state':'New York,NY','span':'1298.4','location':[40.6066, -74.0447]},
+    {'name': 'Golden Gate Bridge','cit/state':'San Francisco and Marin, CA','span':'1280.2','location':[37.8199, -122.4783]},
+    {'name': 'Mackinac Bridge','cit/state':'Mackinaw and St Ignace, MI','span':'1158.0','location':[45.8174, -84.7278]},
+    {'name': 'George Washington Bridge','cit/state':'New York, NY and New Jersey, NJ','span':'1067.0','location':[ 	40.8517, -73.9527]},
+    {'name': 'Tacoma Narrows Bridge','cit/state':'Tacoma and Kitsap, WA','span':'853.44','location':[47.2690, -122.5517]}
+];
+
